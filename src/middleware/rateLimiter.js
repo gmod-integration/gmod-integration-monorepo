f02a5d4377a4932c6830befe5097cf2de70ba7e9
Linventif <linventif@gmail.com>
2023-11-04 22:50:31 +0100

    using rateLimiter of cloudflare

--- src/middleware/rateLimiter.js
+++
@@ -1,7 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-module.exports = rateLimit({
-    windowMs: 60 * 1000, // Time in milliseconds to keep records of requests in memory
-    max: 100, // Max number of requests per IP
-    message: "Too many requests from this IP, please try again in an hour"
-});
