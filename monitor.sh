8989748c3650c2d0310bf79dc78b7a57725eeda4
Linventif <linventif@gmail.com>
2023-11-05 02:28:05 +0100

    refresh status every minutes

--- monitor.sh
+++ monitor.sh
@@ -1,21 +1,8 @@
-#!/bin/bash
-
-# Define the script path and log file
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-LOG_FILE="${SCRIPT_PATH}/logs/monitor.log"
-
-# Function to check if the process is running
-check_process() {
-    # Check if the API process is running
-    if pgrep -f "node src/gm_integration_api.js" > /dev/null
+if pgrep -f "node src/gm_integration_api.js" > /dev/null
     then
-        echo "$(date +%F_%T) - API is running" >> "${LOG_FILE}"
+    # YYYY-MM-DD[space]HH:MM:SS
+        echo "[$(date +%F) $(date +%T)]  API is Running" >> "logs/monitor.log"
     else
-        echo "$(date +%F_%T) - API is not running, starting now..." >> "${LOG_FILE}"
-        cd "${SCRIPT_PATH}"
+        echo "[$(date +%F) $(date +%T)} - API is NOT Running" >> "logs/monitor.log"
         npm run start
     fi
-}
-
-# Run the check_process function
-check_process
