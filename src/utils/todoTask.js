c4e66eb5cad545e9c3ad6aa1dc35006bc4dbe12a
Linventif <linventif@gmail.com>
2023-11-05 02:28:18 +0100

    fix getConnection call

--- src/utils/todoTask.js
+++ src/utils/todoTask.js
@@ -1,4 +1,4 @@
-const getConnection = require('../database/connection');
+const { getConnection } = require('../database/connection');
 
 function addTodoTask(task, data) {
     getConnection().then(connection => {
