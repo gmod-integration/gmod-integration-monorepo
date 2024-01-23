26cdadf36a5bf29fe5be6fbd18a2f4f9b2ae0c84
linventif <linventif@gmail.com>
2024-01-24 00:27:59 +0100

    fix config .env

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 
-const dbConfig = process.env.dbConfig;
-const port_api = 53136;
-const bot_token = process.env.DISCORD_BOT_TOKEN;
-const steamAPI = process.env.STEAM_API_KEY;
-
-module.exports = {
-    dbConfig,
-    port_api,
-    bot_token,
-    steamAPI,
+exports.dbConfig = {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
+    "connectTimeout": 60000,
+    "charset": "utf8mb4_unicode_ci"
 };
+
+exports.port_api = 53136;
+exports.bot_token = process.env.DISCORD_BOT_TOKEN;
+exports.steamAPI = process.env.STEAM_API_KEY;
