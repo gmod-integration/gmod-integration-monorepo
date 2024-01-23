800a3c2f79578b4229680f78281bbdd155e266b9
linventif <linventif@gmail.com>
2024-01-24 00:14:09 +0100

    test

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -1,18 +1,34 @@
 const playerModels = require('../models/playerModels');
 const {badArgument} = require("../utils/tools");
 
-function postScreenshot(req, res) {
-    const { id } = req.headers;
-    const { screenshot, steamID64, options, name } = req.body;
-    const { host } = req.headers;
+async function postScreenshot(req, res) {
+    const {id} = req.headers;
+    const {screenshot, steamID64, options, name} = req.body;
+    const {host} = req.headers;
 
-    if (badArgument([id, screenshot, steamID64, options, host, name])) return res.status(400).json({ error: 'missing_arguments' });
+    if (badArgument([id, screenshot, steamID64, options, host, name])) return res.status(400).json({error: 'missing_arguments'});
 
-    playerModels.postScreenshot(id, screenshot, steamID64, options, host, name).then(() => {
-        res.status(200).send('OK');
+    const steamSummary = await playerModels.getSteamProfile(steamID64);
+    if (!steamSummary) return res.status(400).json({error: 'invalid_steamid64'});
+
+    playerModels.saveScreenshot(id, screenshot, steamID64, options).then((result) => {
+        playerModels.getScreenshotsChannels(id).then((channels) => {
+            channels.forEach((channel) => {
+                // if (channel.adminCmd) return;
+                playerModels.postScreenshot(channel.webhook, channel.token, name || steamSummary.nickname, steamSummary.avatar.medium, `https://${host}/screenshots/${result.filename}`).then(() => {
+                    console.log(`Posted screenshot to ${channel.guildID} - ${channel.channelID}`);
+                }).catch((err) => {
+                    console.log(err);
+                    res.status(500).json({error: 'internal_server_error'});
+                });
+            });
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json({error: 'internal_server_error'});
+        });
     }).catch((err) => {
         console.log(err);
-        res.status(500).json({ error: 'internal_server_error' });
+        res.status(500).json({error: 'internal_server_error'});
     });
 }
 
--- src/models/playerModels.js
+++ src/models/playerModels.js
@@ -3,31 +3,25 @@ const axios = require('axios');
 const {generateToken} = require("../utils/tools");
 const {steamAPI} = require("../config");
 const steamApi = require('steamapi');
+const {getConnection} = require("../database/connection");
 const steam = new steamApi(steamAPI);
 
-function postScreenshot(id, screenshot, steamID64, options, host, name) {
+function getSteamProfile(steamID64) {
+    return new Promise((resolve, reject) => {
+        steam.getUserSummary(steamID64).then((summary) => {
+            resolve(summary);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
+
+function saveScreenshot(id, screenshot, steamID64, options) {
     return new Promise(async (resolve, reject) => {
         // Define the format and the filename
         const format = options.format || 'png';
-        const filename = generateToken(32) + '.' + format;
+        const filename = `${new Date().toISOString().replace(/:/g, '-').replace(/\./g, '-').replace(/T/g, '_')}_${steamID64}_${generateToken(8)}.${format}`;
         const path = `./screenshots/${filename}`;
-
-        const summary = await steam.getUserSummary(steamID64);
-        const avatarUrl = summary.avatar.large;
-
-        const embed = {
-            "embeds": [
-                {
-                    // "title": "New Screenshot",
-                    // "description": "A new screenshot has been uploaded",
-                    "image": {
-                        "url": `https://${host}/screenshots/${filename}`
-                    }
-                }
-            ],
-            "username": name || summary.nickname,
-            "avatar_url": avatarUrl,
-        };
 
         // Remove the Base64 prefix if present
         const base64Data = screenshot.replace(/^data:image\/\w+;base64,/, '');
@@ -42,16 +36,55 @@ function postScreenshot(id, screenshot, steamID64, options, host, name) {
                 reject(err);
             }
 
-            axios.post('https://discord.com/api/webhooks/1197017805430730873/Lw9U1HtbJgd4pEnVUmTkJOTVbkLcrA8oTQZe2t1TGtWSDM08P_wmFIKbYPwBfD7sH5l2', embed).then(function (response) {
-                resolve({path, filename});
-            }).catch(function (error) {
-                console.log(error);
-                reject(error);
+            resolve({path, filename});
+        });
+    });
+}
+
+function getScreenshotsChannels(serverID) {
+    return new Promise((resolve, reject) => {
+        getConnection().then((connection) => {
+            connection.query('SELECT * FROM gm_server_screenshot_channels WHERE serverID = ?', [serverID], (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
             });
+        }).catch((err) => {
+            console.log(err);
+            reject(err);
+        });
+    });
+}
+
+function postScreenshot(webhookID, webhookToken, playerName, avatarUrl, screenshotUrl) {
+    return new Promise(async (resolve, reject) => {
+        const embed = {
+            "embeds": [
+                {
+                    // "title": "New Screenshot",
+                    // "description": "A new screenshot has been uploaded",
+                    "image": {
+                        "url": avatarUrl,
+                    }
+                }
+            ],
+            "username": playerName,
+            "avatar_url": avatarUrl,
+        };
+
+        axios.post(`https://discord.com/api/webhooks/${webhookID}/${webhookToken}`, embed).then(function (response) {
+            resolve();
+        }).catch(function (error) {
+            reject(error);
         });
     });
 }
 
 module.exports = {
-    postScreenshot
+    postScreenshot,
+    saveScreenshot,
+    getSteamProfile,
+    getScreenshotsChannels,
 };
