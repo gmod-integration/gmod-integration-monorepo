1b037be794f073eed0a2dd5bb6c819d35d378097
Linventif <linventif@gmail.com>
2024-02-12 12:51:53 +0100

    edit: only if not already been handled

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,4 +1,8 @@
 module.exports = async (err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+
     console.log(err);
     res.status(500).json({error: 'internal_server_error', message: err.message});
     next();
