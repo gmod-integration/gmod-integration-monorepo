13b1adfcda31464af6daf6cf3dda9db6488aefd3
Linventif <linventif@gmail.com>
2024-02-24 11:53:08 +0100

    add: Server class

--- src/classes/v3/Server.js
+++ src/classes/v3/Server.js
@@ -1,1 +1,19 @@
-// create classes server : id, token,
+const BaseClass = require("./BaseClass");
+
+class Server extends BaseClass {
+    constructor(obj = {}) {
+        super();
+        this.hostname = obj.hostname;
+        this.ip = obj.ip;
+        this.port = obj.port;
+        this.map = obj.map;
+        this.players = obj.players;
+        this.maxPlayers = obj.maxPlayers;
+        this.gameMode = obj.gameMode;
+        this.uptime = obj.uptime;
+    }
+}
+
+module.exports = {
+    Server
+}
