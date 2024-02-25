e2562eda088eb6885c45f2e3b7ebb7ae08700f5d
linventif <linventif@gmail.com>
2024-02-25 19:10:03 +0100

    fix: args checker

--- src/middleware/v3/serverValidator.js
+++ src/middleware/v3/serverValidator.js
@@ -6,10 +6,16 @@ module.exports = (req, res, next) => {
     const {authorization} = req.headers;
 
     if (badArgument([serverID, authorization])) {
-        return res.status(400).json({error: 'missing_arguments', args: {serverID: !!serverID, token: !!token}});
+        return res.status(400).json({
+            error: 'missing_arguments',
+            args: {
+                serverID: !!serverID,
+                authorization: !!authorization
+            }
+        });
     }
+    
     const token = authorization.split(' ')[1];
-
     Server.getServerFromID(serverID).then((server) => {
         if (!server) return res.status(404).json({error: 'server_not_found'});
         if (!server.isValidToken(token)) return res.status(401).json({error: 'unauthorized'});
