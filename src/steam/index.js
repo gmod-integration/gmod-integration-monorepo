014b4fde398f5a93c09fe3bddea0fe4f6e8a9a8a
linventif <linventif@gmail.com>
2024-02-25 02:25:36 +0100

    add: user get avatar

--- src/steam/index.js
+++ src/steam/index.js
@@ -7,6 +7,30 @@ function getSteamApi() {
     return steam;
 }
 
+function getSteamUserSummary(steamID64) {
+    return new Promise(async (resolve, reject) => {
+        const summary = await steam.getUserSummary(steamID64);
+        resolve(summary);
+    });
+}
+
+function getSteamUserAvatars(steamID64) {
+    return new Promise(async (resolve, reject) => {
+        const summary = await steam.getUserSummary(steamID64);
+        resolve(summary.avatar);
+    });
+}
+
+function getSteamUserAvatarLarge(steamID64) {
+    return new Promise(async (resolve, reject) => {
+        const summary = await steam.getUserSummary(steamID64);
+        resolve(summary.avatar.large);
+    });
+}
+
 module.exports = {
-    getSteamApi
+    getSteamApi,
+    getSteamUserSummary,
+    getSteamUserAvatars,
+    getSteamUserAvatarLarge,
 }
