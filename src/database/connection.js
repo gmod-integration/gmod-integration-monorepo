e6b2e5a9c2b87943ef568e50a5f3587d0c9258ce
linventif <linventif@gmail.com>
2024-02-27 08:34:37 +0100

    add: mysql2/promise as new connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,7 @@
 // database/connection.js
 const mysql = require('mysql2');
-const { dbConfig } = require('../config');
-const { gmLog } = require('../utils/logger');
+const {dbConfig} = require('../config');
+const {gmLog} = require('../utils/logger');
 
 let connection;
 
@@ -29,6 +29,36 @@ function getConnection() {
     });
 }
 
+const mysql2Promise = require('mysql2/promise')
+let connectionPromise;
+
+async function getConnectionPromisse() {
+    if (connectionPromise && connectionPromise.state !== 'disconnected') {
+        return connectionPromise;
+    } else {
+        try {
+            connectionPromise = await mysql2Promise.createConnection(dbConfig);
+            gmLog('mysql2Promise', 'Connected to ' + dbConfig.host + ':' + (dbConfig.port || 3306) + '/' + dbConfig.database);
+            // Keep connectionPromise alive by pinging every 60 seconds
+            setInterval(async () => {
+                try {
+                    await connectionPromise.ping();
+                } catch (error) {
+                    console.error(error);
+                    gmLog('mysql2Promise', 'Ping failed');
+                }
+            }, 60000);
+            return connectionPromise;
+        } catch (err) {
+            gmLog('mysql2Promise', 'Connection Error');
+            console.error(err);
+            throw err; // Throw l'erreur pour pouvoir la catcher là où getConnection est appelée
+        }
+    }
+}
+
+
 module.exports = {
-    getConnection
+    getConnection,
+    getConnectionPromisse
 };
