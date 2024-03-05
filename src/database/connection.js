09105182530b2a073ad1aaf3c66535a1b9e177a6
Linventif <linventif@gmail.com>
2024-03-05 04:20:54 +0100

    add: auto create sql table

--- src/database/connection.js
+++ src/database/connection.js
@@ -30,6 +30,9 @@ function getConnection() {
 }
 
 const mysql2Promise = require('mysql2/promise')
+const {writeFileSync, readFileSync} = require("fs");
+const {join} = require("path");
+
 let connectionPromise;
 
 async function getConnectionPromisse() {
@@ -38,25 +41,50 @@ async function getConnectionPromisse() {
     } else {
         try {
             connectionPromise = await mysql2Promise.createConnection(dbConfig);
-            gmLog('mysql2Promise', 'Connected to ' + dbConfig.host + ':' + (dbConfig.port || 3306) + '/' + dbConfig.database);
-            // Keep connectionPromise alive by pinging every 60 seconds
-            setInterval(async () => {
-                try {
-                    await connectionPromise.ping();
-                } catch (error) {
-                    console.error(error);
-                    gmLog('mysql2Promise', 'Ping failed');
-                }
-            }, 60000);
+            gmLog('mysql2', 'Connected to ' + dbConfig.host + ':' + (dbConfig.port || 3306) + '/' + dbConfig.database);
             return connectionPromise;
         } catch (err) {
-            gmLog('mysql2Promise', 'Connection Error');
+            gmLog('mysql2', 'Connection Error');
             console.error(err);
-            throw err; // Throw l'erreur pour pouvoir la catcher là où getConnection est appelée
+            throw err;
         }
     }
 }
 
+// Keep connectionPromise alive by pinging every 60 seconds
+setInterval(async () => {
+    try {
+        await connectionPromise.ping();
+    } catch (error) {
+        console.error(error);
+        gmLog('mysql2', 'Ping failed');
+    }
+}, 60000);
+
+async function executeSqlFile(filePath) {
+    try {
+        const connection = await getConnectionPromisse();
+
+        // Read the SQL file
+        const sqlCommands = readFileSync(filePath, {encoding: 'utf-8'});
+
+        // Split the file content by SQL command delimiter (e.g., `;` for MySQL)
+        const commands = sqlCommands.split(';').map(command => command.trim()).filter(command => command.length);
+
+        // Execute each SQL command
+        for (let command of commands) {
+            await connection.execute(command);
+        }
+
+        await connection.end();
+    } catch (error) {
+        console.error('Error executing SQL file:', error);
+    }
+}
+
+executeSqlFile('./src/database/shema.sql').then(() => {
+    gmLog('mysql2', 'Database schema created');
+});
 
 module.exports = {
     getConnection,
