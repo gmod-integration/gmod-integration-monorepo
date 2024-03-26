50a86393e5f5d331c4ab5d7010e70fdf479de76f
linventif <linventif@gmail.com>
2024-03-27 00:13:48 +0100

    add: handle commands

--- src/discord/index.js
+++ src/discord/index.js
@@ -1,4 +1,4 @@
-const {Client, GatewayIntentBits} = require('discord.js');
+const {Client, GatewayIntentBits, Collection, Events} = require('discord.js');
 const client = new Client({
     intents: [
         GatewayIntentBits.AutoModerationConfiguration,
@@ -22,12 +22,85 @@ const {
 } = require('../config/index');
 const {readdirSync} = require("fs");
 const {gmLog} = require("../utils/logger");
+const {join} = require("path");
 
 client.on('ready', () => {
     gmLog('discord', 'Connected to Discord');
 });
 
 client.login(token).catch(console.error);
+
+module.exports = {
+    getClient,
+    updateGuildUserPseudo,
+};
+
+// Load Events
+const eventFiles = readdirSync('./src/discord/events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.name) {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+// Load Slash Commands
+client.commands = new Collection();
+const foldersPath = join(__dirname, 'commands');
+const commandFolders = readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = join(foldersPath, folder);
+    const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const filePath = join(commandsPath, file);
+        const command = require(filePath);
+
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+            console.log(`[INFO] Loaded command ${command.data.name} from ${filePath}`);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+    console.log(interaction);
+    if (interaction.isChatInputCommand()) {
+        const command = client.commands.get(interaction.commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({
+                    content: 'There was an error while executing this command!',
+                    ephemeral: true
+                });
+            } else {
+                await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
+            }
+        }
+    } else if (interaction.isAutocomplete()) {
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        try {
+            await command.autocomplete(interaction);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+});
 
 async function getClient() {
     if (client.readyAt) {
@@ -79,18 +152,4 @@ function updateGuildUserPseudo(guildID, userID, pseudo) {
                 .catch(reject);
         }).catch(reject);
     });
-}
-
-module.exports = {
-    getClient,
-    updateGuildUserPseudo,
-};
-
-// Load Events
-const eventFiles = readdirSync('./src/discord/events').filter(file => file.endsWith('.js'));
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
-    if (event.name) {
-        client.on(event.name, (...args) => event.execute(...args, client));
-    }
 }
