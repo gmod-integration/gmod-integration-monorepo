c9f5c156df5328c8b4a59a1a9fe1a931a2f62802
linventif <linventif@gmail.com>
2024-04-13 18:31:19 +0200

    remove: stripe webhooks

--- src/routes/webhooks/_webhooksRoutes.js
+++ src/routes/webhooks/_webhooksRoutes.js
@@ -1,12 +1,9 @@
 import express from 'express';
 import gmodStoreValidatorMiddleware from '../../middleware/webhooks/gmodStoreValidator.js';
 import gmodStoreRoutes from './gmodstore/_gmodStoreRoutes.js';
-import stripeValidatorMiddleware from '../../middleware/webhooks/stripeValidator.js';
-import stripeRoutes from './stripe/_stripeRoutes.js';
 
 const router = express.Router();
 
 router.use('/gmod-store', gmodStoreValidatorMiddleware, gmodStoreRoutes);
-router.use('/stripe', stripeValidatorMiddleware, stripeRoutes);
 
 export default router;
