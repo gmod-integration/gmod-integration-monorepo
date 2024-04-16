83868f09f2c83677fac3d056d0ef3aad85bcb39f
linventif <linventif@gmail.com>
2024-04-16 14:15:27 +0200

    add: context menu + command push

---
+++ src/discord/discord_push_context.js
@@ -0,0 +1,16 @@
+import { ApplicationCommandType, ContextMenuCommandBuilder, REST, Routes } from 'discord.js';
+import { discordConfig } from '../config/index.js';
+
+const commandsData = [new ContextMenuCommandBuilder().setName('Show Profile').setType(ApplicationCommandType.USER)];
+
+const rest = new REST().setToken(discordConfig.botToken);
+try {
+  console.log('Started refreshing application.');
+
+  await rest.put(Routes.applicationCommands(discordConfig.clientID), {
+    body: commandsData,  });
+
+  console.log('Successfully reloaded application.');
+} catch (error) {
+  console.error(error);
+}
