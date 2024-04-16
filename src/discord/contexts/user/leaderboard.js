35ad4ac0be2918eee1fb28fbefd701aa5a0e95ca
linventif <linventif@gmail.com>
2024-04-16 17:32:12 +0200

    fix: context action name

--- src/discord/contexts/user/leaderboard.js
+++ src/discord/contexts/user/leaderboard.js
@@ -1,7 +1,7 @@
 import { ApplicationCommandType, ContextMenuCommandBuilder } from 'discord.js';
 
 export default {
-  data: new ContextMenuCommandBuilder().setName('Lfeeaderboard').setType(ApplicationCommandType.User),
+  data: new ContextMenuCommandBuilder().setName('Leaderboard').setType(ApplicationCommandType.User),
   category: 'user',
   async execute(interaction) {
     // todo
