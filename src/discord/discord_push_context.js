a8e38ea220d84bdc3b6833ac8c4615f02d2c84fb
linventif <linventif@gmail.com>
2024-04-16 17:31:55 +0200

    add: better push commands and context

--- src/discord/discord_push_context.js
+++ src/discord/discord_push_context.js
@@ -1,21 +1,54 @@
-import { ApplicationCommandType, ContextMenuCommandBuilder, REST, Routes } from 'discord.js';
+import { ApplicationCommandType, REST, Routes } from 'discord.js';
 import { discordConfig } from '../config/index.js';
+import { join } from 'path';
+import { readdir } from 'fs/promises';
 
-const commandsData = [
-  new ContextMenuCommandBuilder().setName('Profile').setType(ApplicationCommandType.User),
-  new ContextMenuCommandBuilder().setName('Statistics').setType(ApplicationCommandType.User),
-  new ContextMenuCommandBuilder().setName('Leaderboard').setType(ApplicationCommandType.User),
-];
+const commandsData = [];
+
+async function loadCommands(dirPath, type) {
+  try {
+    const foldersPath = join(process.cwd(), dirPath);
+    const folders = await readdir(foldersPath);
+
+    for (const folder of folders) {
+      const commandsPath = join(foldersPath, folder);
+      const commandFiles = (await readdir(commandsPath)).filter((file) => file.endsWith('.js'));
+
+      for (const file of commandFiles) {
+        try {
+          const filePath = join(commandsPath, file);
+          const command = await import(filePath);
+          if (command.default && command.default.data) {
+            commandsData.push(command.default.data.toJSON());
+            console.log(`[INFO] Pushed ${type} ${command.default.data.name} from ${filePath}`);
+          } else {
+            console.log(`[WARNING] The ${type} at ${filePath} is missing a required "data" or "execute" property.`);
+          }
+        } catch (error) {
+          console.error(`[ERROR] Failed to load ${type} from ${file}: ${error}`);
+        }
+      }
+    }
+  } catch (error) {
+    console.error(`[ERROR] Failed to read directory ${dirPath}: ${error}`);
+  }
+}
 
 const rest = new REST().setToken(discordConfig.botToken);
 try {
-  console.log('Started refreshing application.');
+  console.log('[INFO] Started reloading application.');
+
+  await loadCommands('src/discord/contexts', 'Context');
+  await loadCommands('src/discord/commands', 'Command');
+
+  console.log(commandsData);
 
   await rest.put(Routes.applicationCommands(discordConfig.clientID), {
     body: commandsData,
   });
 
-  console.log('Successfully reloaded application.');
+  console.log('[INFO] Successfully reloaded application.');
 } catch (error) {
+  console.error('[ERROR] Failed to reload application.');
   console.error(error);
 }
