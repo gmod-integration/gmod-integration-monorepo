4a348113b81b601ecd90b592162cd181b1ca2926
linventif <linventif@gmail.com>
2024-05-07 05:32:04 +0200

    add: sequelize

---
+++ src/database/sequelize.js
@@ -0,0 +1,20 @@
+import { Sequelize } from 'sequelize';
+import { databaseConfig } from '../config/index.js';
+import { gmLog } from '../utils/logger.js';
+
+const sequelize = new Sequelize({
+  dialect: 'mariadb',
+  host: databaseConfig.host,
+  username: databaseConfig.user,
+  password: databaseConfig.password,
+  database: databaseConfig.database,
+});
+
+try {
+  await sequelize.authenticate();
+  gmLog('sequelize', 'Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+export default sequelize;
