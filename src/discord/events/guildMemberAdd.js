b136739c029e1109977408613ebe38f972b305e6
linventif <linventif@gmail.com>
2024-05-09 01:14:52 +0200

    fix: duble create in db

--- src/discord/events/guildMemberAdd.js
+++ src/discord/events/guildMemberAdd.js
@@ -1,8 +1,13 @@
 import gm_guild from '../../database/shema/gm_guild.js';
+import { discordConfig } from '../../config/index.js';
 
 export default {
   name: 'guildMemberAdd',
   async execute(add_info) {
+    if (add_info.member.user.id === discordConfig.clientID) {
+      return;
+    }
+
     const editedGuild = await gm_guild.findOne({
       where: {
         guild: add_info.guild.id,
