1fb50ddcde5cd9ab249b5d7cae62ac1e27c602d4
linventif <linventif@gmail.com>
2024-05-19 14:38:53 +0200

    add: gm_status schema

---
+++ src/database/shema/gm_status.js
@@ -0,0 +1,47 @@
+import sequelize from '../sequelize.js';
+import { DataTypes, Model } from 'sequelize';
+import { gmLog } from '../../utils/logger.js';
+
+class gm_status extends Model {
+  // Extend the class here
+}
+
+gm_status.init(
+  {
+    server: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'gm_server',
+        key: 'id',
+      },
+    },
+    message: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: '',
+    },
+    channel: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: '',
+    },
+  },
+  {
+    sequelize,
+    modelName: 'gm_status',
+    tableName: 'gm_status',
+    timestamps: true,
+  },
+);
+
+gm_status
+  .sync({ alter: true })
+  .then(() => {
+    gmLog('sequelize', 'Table created: gm_status');
+  })
+  .catch((error) => {
+    console.error('Error creating gm_status table:', error);
+  });
+
+export default gm_status;
