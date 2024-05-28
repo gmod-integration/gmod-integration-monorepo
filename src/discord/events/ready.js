b4a9a3e8cf13062aec8c065ef1111b30446dfc80
linventif <linventif@gmail.com>
2024-05-28 03:37:20 +0200

    fix: stat count + bot status

--- src/discord/events/ready.js
+++ src/discord/events/ready.js
@@ -1,5 +1,7 @@
 import { getClient } from '../index.js';
 import gm_guild from '../../database/schema/gm_guild.js';
+import { getStats } from '../../models/v3/mainModels.js';
+import { ActivityType } from 'discord.js';
 
 export default {
   name: 'ready',
@@ -28,5 +30,53 @@ export default {
         });
       }
     }
+
+    const botStatusList = [
+      async function userCount(stat) {
+        return client.user.setPresence({
+          activities: [
+            {
+              name: `${stat.user.toLocaleString()} users`,
+              type: ActivityType.Watching,
+            },
+          ],
+        });
+      },
+      function serverCount(stat) {
+        return client.user.setPresence({
+          activities: [
+            {
+              name: `${stat.server.toLocaleString()} servers`,
+              type: ActivityType.Watching,
+            },
+          ],
+        });
+      },
+      function version() {
+        return client.user.setPresence({
+          activities: [
+            {
+              name: 'v0.3.4',
+              type: ActivityType.Watching,
+            },
+          ],
+        });
+      },
+    ];
+
+    let lastStatusID = 0;
+
+    async function updateStatus() {
+      const stats = await getStats();
+      const status = botStatusList[lastStatusID];
+      lastStatusID = (lastStatusID + 1) % botStatusList.length;
+      const rtn = await status(stats);
+      console.log(`Status updated`);
+      console.log(rtn);
+    }
+
+    // every 30s update the bot status
+    setInterval(updateStatus, 30000);
+    await updateStatus();
   },
 };
--- src/models/v3/mainModels.js
+++ src/models/v3/mainModels.js
@@ -1,15 +1,30 @@
 import { getConnectionPromise } from '../../database/connection.js';
+import redis from '../../redis/index.js';
+import gm_guild from '../../database/schema/gm_guild.js';
 
 export async function getStats() {
+  const redisKey = 'stats';
+  const redisStat = await redis.get(redisKey);
+  if (redisStat !== null) {
+    return JSON.parse(redisStat);
+  }
+
   const connection = await getConnectionPromise();
   const [rows] = await connection.query('SELECT COUNT(*) FROM gm_user WHERE steam IS NOT NULL');
   const [rows2] = await connection.query('SELECT COUNT(*) FROM users');
-  const [rows3] = await connection.query('SELECT * FROM gm_stat_discord');
   const [rows4] = await connection.query('SELECT COUNT(*) FROM gm_server');
-  return {
+
+  const memberCount = await gm_guild.sum('member');
+  const guildCount = await gm_guild.count();
+
+  const stats = {
     verifyUser: rows[0]['COUNT(*)'],
-    user: rows3[0]['guildMembers'] + rows2[0]['COUNT(*)'],
-    guild: rows3[0]['guild'],
+    user: memberCount + rows2[0]['COUNT(*)'],
+    guild: guildCount,
     server: rows4[0]['COUNT(*)'],
   };
+
+  await redis.set(redisKey, JSON.stringify(stats), 'EX', 120);
+
+  return stats;
 }
