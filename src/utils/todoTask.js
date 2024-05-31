6bf453648d60204987cf8efc9e8218b8cec86b1c
linventif <linventif@gmail.com>
2024-06-01 00:11:49 +0200

    remove: un-use file

--- src/utils/todoTask.js
+++
@@ -1,8 +0,0 @@
-import { getConnectionPromise } from '../database/connection.js';
-
-export async function addTodoTask(task, data) {
-  const connection = await getConnectionPromise();
-  connection.query('INSERT INTO gm_todo_task (task, data) VALUES (?, ?)', [task, data], (error) => {
-    if (error) throw error;
-  });
-}
