f9fb72fbd916e08a0cb631f791c3c4a6a936723b
linventif <linventif@gmail.com>
2024-06-09 18:14:06 +0200

    add: static premium Guild is now in db

---
+++ src/database/schema/PremiumGuild.js
@@ -0,0 +1,40 @@
+import sequelize from '../sequelize.js';
+import { DataTypes, Model } from 'sequelize';
+import { gmLog } from '../../utils/logger.js';
+
+class PremiumGuild extends Model {
+  // Extend the class here
+}
+
+PremiumGuild.init(
+  {
+    guildID: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    transaction: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    buyer: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'gm_guild_premium',
+    tableName: 'gm_guild_premium',
+    timestamps: true,
+  },
+);
+
+PremiumGuild.sync({ alter: true })
+  .then(() => {
+    gmLog('sequelize', 'Table created: gm_guild_premium');
+  })
+  .catch((error) => {
+    console.error('Error creating gm_guild_premium table:', error);
+  });
+
+export default PremiumGuild;
