d615de10d7df80c080253d8a573594234b7ad309
linventif <linventif@gmail.com>
2024-06-22 12:10:09 +0200

    refactor: using sequelize GuildNotVerifiedRole

--- src/discord/events/channelCreate.js
+++ src/discord/events/channelCreate.js
@@ -1,19 +1,20 @@
-import { getConnectionPromise } from '../../database/connection.js';
 import { gmLog } from '../../utils/logger.js';
+import GuildNotVerifiedRole from '../../database/schema/GuildNotVerifiedRole.js';
 
 export default {
   name: 'channelCreate',
   async execute(channel) {
     const guild = channel.guild;
     gmLog('event', `Channel created in guild: ${guild.name}`);
-    
-    const connection = await getConnectionPromise();
-    const query = `SELECT *
-                   FROM gm_role_auto
-                   WHERE guild = ?`;
-    const [results] = await connection.query(query, [guild.id]);
-    if (results && results[0]) {
-      const role = guild.roles.cache.get(results[0].id);
+
+    const notVerifiedRole = await GuildNotVerifiedRole.findOne({
+      where: {
+        guild: guild.id,
+      },
+    });
+
+    if (notVerifiedRole) {
+      const role = guild.roles.cache.get(notVerifiedRole.id);
       if (!role) return;
       channel.permissionOverwrites.edit(role, { ViewChannel: false });
     }
