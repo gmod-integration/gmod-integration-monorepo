ca9f12afe19d0aee644c82638a07476604c1e984
linventif <linventif@gmail.com>
2024-07-01 08:26:49 +0200

    fix: wss auth

--- src/websockets/index.js
+++ src/websockets/index.js
@@ -13,53 +13,43 @@ const wss = new WebSocketServer({
   port: serverConfig.ports.websocket,
   clientTracking: true,
   verifyClient: async (info, cb) => {
-    try {
-      let authorized = false;
-
-      const { id, token } = info.req.headers;
-      if (id && token) {
-        const server = await getServerFromID(id);
-        if (server && server.isValidToken(token)) {
-          gmLog('websocket', 'Authorized server ' + id);
-          authorized = true;
-        }
+    const { id, token } = info.req.headers;
+    if (id && token) {
+      const server = await getServerFromID(id);
+      if (server && server.isValidToken(token)) {
+        gmLog('websocket', 'Authorized server ' + id);
+        return cb(true);
       }
+    }
 
-      if (
-        info.req.url.includes('discordID') &&
-        info.req.url.includes('token') &&
-        info.req.url.includes('guildID') &&
-        info.req.url.includes('serverID') &&
-        info.req.url.includes('action')
-      ) {
-        const args = new URLSearchParams(info.req.url.split('?')[1].split('/').join('&'));
-        const authToken = args.get('token');
-        const discordID = args.get('discordID');
-        const guildID = args.get('guildID');
-        const serverID = args.get('serverID');
-        const action = args.get('action');
+    if (
+      info.req.url.includes('discordID') &&
+      info.req.url.includes('token') &&
+      info.req.url.includes('guildID') &&
+      info.req.url.includes('serverID') &&
+      info.req.url.includes('action')
+    ) {
+      const args = new URLSearchParams(info.req.url.split('?')[1].split('/').join('&'));
+      const authToken = args.get('token');
+      const discordID = args.get('discordID');
+      const guildID = args.get('guildID');
+      const serverID = args.get('serverID');
+      const action = args.get('action');
 
-        if (discordID && authToken && guildID && serverID && action) {
-          const user = await getPanelUserFromDiscordID(discordID);
-          if (user && user.authAllowed(authToken) && (await user.isAdminOfGuild(guildID))) {
-            const server = await getServerFromID(serverID);
-            if (server && server.getGuildID() !== serverID) {
-              gmLog('websocket', 'Authorized client ' + discordID);
-              authorized = true;
-            }
+      if (discordID && authToken && guildID && serverID && action) {
+        const user = await getPanelUserFromDiscordID(discordID);
+        if (user && user.authAllowed(authToken) && (await user.isAdminOfGuild(guildID))) {
+          const server = await getServerFromID(serverID);
+          if (server && server.getGuildID() !== serverID) {
+            gmLog('websocket', 'Authorized client ' + discordID);
+            return cb(true);
           }
         }
       }
-
-      if (!authorized) {
-        gmLog('websocket', 'Unauthorized connection');
-        return cb(false, 401, 'Unauthorized');
-      }
-    } catch (error) {
-      gmLog('websocket', 'Error during connection verification');
-      console.error(error);
-      return cb(false, 500, 'Internal Server Error');
     }
+
+    gmLog('websocket', 'Unauthorized connection');
+    return cb(false, 401, 'Unauthorized');
   },
 });
 
