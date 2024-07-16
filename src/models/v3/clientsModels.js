4535dc0e565d0d307dc0a084f7b19f263a3ba9fe
linventif <linventif@gmail.com>
2024-07-16 13:47:53 +0200

    fix: use custom bot instance

--- src/models/v3/clientsModels.js
+++ src/models/v3/clientsModels.js
@@ -3,7 +3,6 @@ import fs from 'fs';
 import { serverConfig } from '../../config/index.js';
 import { WebhookClient } from 'discord.js';
 import { getSteamUserAvatarLarge } from '../../steam/index.js';
-import { getMainClient } from '../../discord/index.js';
 
 export function saveScreenshot(screenshot, captureData, player) {
   return new Promise(async (resolve, reject) => {
@@ -36,7 +35,7 @@ export async function sendScreenshotToDiscord(path, filename, player, server) {
   }
 
   // TODO Better code
-  const dscClient = await getMainClient();
+  const dscClient = await server.getBotInstance();
 
   const dscGuild = dscClient.guilds.cache.get(channelInfo.guild);
   if (!dscGuild) {
