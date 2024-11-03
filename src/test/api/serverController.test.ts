f49880a3785d81a0d2e3c4f6c7ce0762b5065cfa
linventif <linventif@gmail.com>
2024-11-03 05:56:01 +0100

    Feat: add server test

--- src/test/api/serverController.test.ts
+++ src/test/api/serverController.test.ts
@@ -1,15 +1,35 @@
 import { test } from 'node:test';
 import assert from 'assert';
-import { getTestUri } from '../config.test';
+import testConfig, { getTestUri } from '../config.test';
+import prisma from '../../prisma';
 
-// Version Info
-test('GET /v3', async (t) => {
-  const response = await fetch(getTestUri('/v3'));
+// Server Info
+test('GET /v3/servers/:serverID', async (t) => {
+  const response = await fetch(getTestUri('/v3/servers/:serverID'), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + testConfig.server.token,
+    },
+  });
   assert.strictEqual(response.status, 200);
 });
 
-// Stats
-test('GET /v3/stats', async (t) => {
-  const response = await fetch(getTestUri('/v3/stats'));
+// Server Generate Public Token
+test('POST /v3/servers/:serverID/public-token', async (t) => {
+  const response = await fetch(getTestUri('/v3/servers/:serverID/public-token'), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + testConfig.server.token,
+    },
+  });
   assert.strictEqual(response.status, 200);
+  const prismaData = await prisma.gm_server.findFirst({
+    where: {
+      id: testConfig.server.id,
+    },
+  });
+  const responseJson = await response.json();
+  assert.strictEqual(prismaData?.publicTempToken, responseJson.publicTempToken);
 });
