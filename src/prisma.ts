b0d00f2e41580716adbbf88e971702ee6b54ac72
linventif <linventif@gmail.com>
2024-11-06 04:04:22 +0100

    Fix: prisma handle error

--- src/prisma.ts
+++ src/prisma.ts
@@ -2,4 +2,9 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+// catch all unhandled errors
+prisma.$on('error', (e) => {
+  console.error(e);
+});
+
 export default prisma;
