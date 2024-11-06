1df11dd0ebc7c81c84813b2de82b1a68f345bd9e
linventif <linventif@gmail.com>
2024-11-06 05:06:41 +0100

    Fix: skip prisma error ?

--- src/prisma.ts
+++ src/prisma.ts
@@ -2,9 +2,4 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-// catch all unhandled errors
-prisma.$on('error', (e: any) => {
-  console.error(e);
-});
-
 export default prisma;
