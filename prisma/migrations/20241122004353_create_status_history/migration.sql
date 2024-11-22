74e10a9c8754cc0bab56ea6075481e17eec9a068
linventif <linventif@gmail.com>
2024-11-22 01:49:18 +0100

    Fix: prisma ?

--- prisma/migrations/20241122004353_create_status_history/migration.sql
+++ prisma/migrations/20241122004353_create_status_history/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `customValues` on the `gm_server_stat_session` table. The data in that column could be lost. The data in that column will be cast from `Text` to `Json`.
-
-*/
 -- AlterTable
 ALTER TABLE `banUsers` MODIFY `unbanDate` DATETIME(3) NOT NULL DEFAULT (CURRENT_TIMESTAMP + INTERVAL 2 YEAR);
 
@@ -12,12 +6,6 @@ ALTER TABLE `gm_discordToken` MODIFY `expirationDate` DATETIME(3) NOT NULL DEFAU
 
 -- AlterTable
 ALTER TABLE `gm_panelToken` MODIFY `expirationDate` DATETIME(3) NOT NULL DEFAULT (CURRENT_TIMESTAMP + INTERVAL 7 DAY);
-
--- AlterTable
-ALTER TABLE `gm_server_stat_session` MODIFY `customValues` JSON NULL;
-
--- AlterTable
-ALTER TABLE `gm_server_status` ADD COLUMN `playersList` JSON NULL;
 
 -- AlterTable
 ALTER TABLE `gm_user` MODIFY `token_expires` DATETIME(3) NULL DEFAULT (CURRENT_TIMESTAMP + INTERVAL 7 DAY);
