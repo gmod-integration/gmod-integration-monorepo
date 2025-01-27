070805d4aef4719eb1dcead374c6ff021b5f5d71
linventif <linventif@gmail.com>
2025-01-27 22:09:00 +0100

    Fix: forgot to add userAdminControllers

---
+++ src/controllers/v3/usersAdminControllers.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import prisma from '../../prisma.js';
+
+export async function getAllPanelUsers(req: Request, res: Response) {
+  const users = await prisma.gm_panelToken.findMany({
+    where: {
+      expirationDate: {
+        gt: new Date(),
+      },
+    },
+  });
+  return res.status(200).json(users);
+}
