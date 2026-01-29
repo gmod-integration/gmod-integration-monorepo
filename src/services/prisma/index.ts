bbdea35925d8852f2bbcebed5d0b8322f991cc86
linventif <gregoire.becue@pm.me>
2026-01-29 11:31:12 +0100

    npm prisma upgrade from 6 to 7

--- src/services/prisma/index.ts
+++ src/services/prisma/index.ts
@@ -1,7 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../prisma/generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { connectToMongoDB } from '../mongo/index.js';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   await connectToMongoDB();
