cae38504f91176a99f2d8ad20690ea1369015777
linventif <gregoire.becue@pm.me>
2026-03-17 11:34:55 +0100

    chore: add linting and formatting scripts, configure ESLint and Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
+
+export default [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.turbo/**',
+      '**/coverage/**',
+      '**/logs/**',
+      'packages/infra-prisma/generated/**',
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      'no-console': 'off',
+      'prefer-const': 'warn',
+      'no-case-declarations': 'warn',
+      'no-control-regex': 'warn',
+      'no-useless-escape': 'warn',
+      'no-async-promise-executor': 'warn',
+    },
+  },
+  prettier,
+]
